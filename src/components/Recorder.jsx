2b552c21f3bcd3f4d52c628b2ddfa50c46f0a249
Mahammad Sami Khaji <kmahammadsami959@gmail.com>
2025-02-10 15:44:06 +0530

    doing gdrive

--- src/components/Recorder.jsx
+++ src/components/Recorder.jsx
@@ -14,7 +14,7 @@ function Recorder({ selectedLanguage }) {
   // Play greeting at the start
   useEffect(() => {
     if (selectedLanguage) {
-      const greetingAudio = new Audio(`${process.env.PUBLIC_URL}/Audio/${selectedLanguage}.mp3`);
+      const greetingAudio = new Audio(`/voice-recorder-app/Audio/${selectedLanguage}.mp3`);
       greetingAudioRef.current = greetingAudio;
       greetingAudio.play().catch((error) => {
         console.error('Audio playback failed:', error);
