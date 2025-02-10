84ff826da523175efe962c78229dc4db4ac689ae
Mahammad Sami Khaji <kmahammadsami959@gmail.com>
2025-02-10 15:39:31 +0530

    doing gdrive

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/voice-recorder-app/',  // Correct path for GitHub Pages
+  base: '/voice-recoreder-app/',  // Correct path for GitHub Pages
   plugins: [react()]
 });
